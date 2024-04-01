1b38e827167fd96ff6316f3adff98400d066d2fd
Jonathan Levi <yonilevi0013@gmail.com>
2024-04-01 22:42:04 +0300

    wip

--- main.tf
+++ main.tf
@@ -44,11 +44,32 @@ resource "azurerm_subnet" "endpoint" {
   enforce_private_link_endpoint_network_policies = true
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "clalit-storage-account" {
   name                     = "clalitaccount"
   resource_group_name      = azurerm_resource_group.clalit-rg.name
   location                 = azurerm_resource_group.clalit-rg.location
   account_tier             = "Standard"
   account_kind             = "StorageV2"
   account_replication_type = "LRS"
+}
+
+
+resource "azurerm_app_service_plan" "clalit-app-svc-plan" {
+  name                = "azure-functions-test-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_function_app" "example" {
+  name                       = "test-azure-functions"
+  location                   = azurerm_resource_group.clalit-rg.location
+  resource_group_name        = azurerm_resource_group.clalit-rg.name
+  app_service_plan_id        = azurerm_app_service_plan.clalit-app-svc-plan.id
+  storage_account_name       = azurerm_storage_account.clalit-storage-account.name
+  storage_account_access_key = azurerm_storage_account.clalit-storage-account.primary_access_key
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "virtual_network_name" {
   value = azurerm_virtual_network.clalit-vn
 }
+
+output "clalit-storage-account" {
+    value = azurerm_storage_account.clalit-storage-account.id
+  
+}
