16286ff79db9011c8c0b08eeade9bda7a3c9c173
Jonathan Levi <yonilevi0013@gmail.com>
2024-04-01 22:25:32 +0300

    gitignore file

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "clalit-rg" {
   name     = "example-resources"
   location = "West Europe"
 }
@@ -16,69 +16,35 @@ resource "azurerm_resource_group" "example" {
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  name                = "clalit-network"
+  resource_group_name = azurerm_resource_group.clalit-rg.name
+  location            = azurerm_resource_group.clalit-rg.location
   address_space       = ["10.0.0.0/16"]
 }
 
 
 resource "azurerm_subnet" "service" {
-  name                 = "service"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
+  name                                          = "service"
+  resource_group_name                           = azurerm_resource_group.clalit-rg-rg.name
+  virtual_network_name                          = azurerm_virtual_network.clalit-rg.name
+  address_prefixes                              = ["10.0.1.0/24"]
   enforce_private_link_service_network_policies = true
 }
 
 resource "azurerm_subnet" "endpoint" {
   name                 = "endpoint"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.clalit-rg.name
+  virtual_network_name = azurerm_virtual_network.clalit-rg.name
   address_prefixes     = ["10.0.2.0/24"]
 
   enforce_private_link_endpoint_network_policies = true
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageaccount"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  name                     = "clalit-account"
+  resource_group_name      = azurerm_resource_group.clalit-rg.name
+  location                 = azurerm_resource_group.clalit-rg.location
   account_tier             = "Standard"
   account_kind             = "StorageV2"
   account_replication_type = "LRS"
-}
-
-
-resource "azurerm_monitor_aad_diagnostic_setting" "example" {
-  name               = "setting1"
-  storage_account_id = azurerm_storage_account.example.id
-  enabled_log {
-    category = "SignInLogs"
-    retention_policy {
-      enabled = true
-      days    = 1
-    }
-  }
-  enabled_log {
-    category = "AuditLogs"
-    retention_policy {
-      enabled = true
-      days    = 1
-    }
-  }
-  enabled_log {
-    category = "NonInteractiveUserSignInLogs"
-    retention_policy {
-      enabled = true
-      days    = 1
-    }
-  }
-  enabled_log {
-    category = "ServicePrincipalSignInLogs"
-    retention_policy {
-      enabled = true
-      days    = 1
-    }
-  }
 }
