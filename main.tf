1435e1e7e858e8e6d5412b223c6286238d8082a2
Jonathan Levi <yonilevi0013@gmail.com>
2024-04-01 22:31:12 +0300

    wip

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_subnet" "endpoint" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "clalit-account"
+  name                     = "clalitaccount"
   resource_group_name      = azurerm_resource_group.clalit-rg.name
   location                 = azurerm_resource_group.clalit-rg.location
   account_tier             = "Standard"
